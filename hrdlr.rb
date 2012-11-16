1d9e2709d92fe068a905510b86750e2a071cca2d
Jason Weathered, Nate Kane, and Odin Dutton <dev+jason+nate+odin@ennova.com.au>
2012-11-17 10:38:26 +1100

    Handle out of frame rendering.

--- hrdlr.rb
+++ hrdlr.rb
@@ -57,14 +57,36 @@ class Sprite
 end
 
 class Frame
+  attr_reader :rows
+
   def initialize(width, height)
     @rows = height.times.map { ' ' * width }
   end
 
   def draw(x, y, sprite)
     lines = sprite.split("\n")
+
+    # overlap left
+    if x < 0
+      lines.map! do |line|
+        if line.size >= -x
+          line[-x..-1]
+        else
+          ' ' * line.size
+        end
+      end
+      x = 0
+    end
+
+    # overlap right
+    lines.map! do |line|
+      line[0..@rows.first.size-x-1]
+    end
+
     lines.each_with_index do |line, i|
-      @rows[y+i][x..x+line.size-1] = line
+      if line.size > 0 && x+line.size <= @rows.first.size
+        @rows[y+i][x..x+line.size-1] = line
+      end
     end
   end
 
---
+++ test/frame_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class FrameTest < MiniTest::Unit::TestCase
+  def test_draw_in_range
+    frame = Frame.new 5, 1
+    frame.draw 1, 0, 'foo'
+    assert_equal [' foo '], frame.rows
+  end
+
+  def test_draw_overlap_left
+    frame = Frame.new 5, 1
+    frame.draw -1, 0, 'foo'
+    assert_equal ['oo   '], frame.rows
+  end
+
+  def test_draw_overlap_right
+    frame = Frame.new 5, 1
+    frame.draw 3, 0, 'foo'
+    assert_equal ['   fo'], frame.rows
+  end
+
+  def test_draw_overlap_both
+    frame = Frame.new 3, 1
+    frame.draw -3, 0, 'abcdefghi'
+    assert_equal ['def'], frame.rows
+  end
+
+  def test_draw_hidden_left
+    frame = Frame.new 5, 1
+    frame.draw -5, 0, 'foo'
+    assert_equal ['     '], frame.rows
+  end
+
+  def test_draw_hidden_right
+    frame = Frame.new 5, 1
+    frame.draw 10, 0, 'foo'
+    assert_equal ['     '], frame.rows
+  end
+
+  def test_draw_left_edge
+    frame = Frame.new 5, 1
+    frame.draw -1, 0, 'f'
+    assert_equal ['     '], frame.rows
+  end
+
+  def test_draw_right_edge
+    frame = Frame.new 5, 1
+    frame.draw 5, 0, 'f'
+    assert_equal ['     '], frame.rows
+  end
+end
