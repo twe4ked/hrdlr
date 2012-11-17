9e838229868eaf08d0e8ff7df255b0068a6bb638
Jason Weathered, Nate Kane, and Odin Dutton <dev+jason+nate+odin@ennova.com.au>
2012-11-17 17:12:02 +1100

    Split render method into smaller methods.

--- lib/game.rb
+++ lib/game.rb
@@ -19,20 +19,41 @@ class Game
 
   def render
     frame = Frame.new 80, 6
-    viewport_x = player.x - 4
+
+    draw_track(frame)
+    draw_player(frame)
+    draw_hurdles(frame)
+    draw_title(frame)
+
+    frame.render
+  end
+
+  def draw_track(frame)
     frame.draw 0, 0, Sprite.track_line
     frame.draw 0, 5, Sprite.track_line
+  end
+
+  def draw_player(frame)
     frame.draw player.x-viewport_x, 2-player.y, Sprite.player(player.state)
+  end
+
+  def draw_hurdles(frame)
     track.get_hurdles(viewport_x...viewport_x+80).each do |hurdle_x|
       frame.draw hurdle_x-viewport_x, 4, Sprite.hurdle
     end
+  end
+
+  def draw_title(frame)
     if (0...32).cover?(tick_count)
       frame.draw_center 1, 'HURDLURR!!!'
       if tick_count % 8 >= 4
         frame.draw_center 2, 'Press <Space> to jump!'
       end
     end
-    frame.render
+  end
+
+  def viewport_x
+    player.x - 4
   end
 
   def joystick
