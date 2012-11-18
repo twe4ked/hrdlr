0798969e8dbbef9beab1a74cc7bc026cb41cc594
Jason Weathered, Nate Kane, and Odin Dutton <dev+jason+nate+odin@ennova.com.au>
2012-11-18 19:10:12 +1100

    Make dino rampage harder to get.

--- lib/track.rb
+++ lib/track.rb
@@ -6,6 +6,6 @@ class Track
   def initialize
     @hurdles = Items.new 50, 10..20
     @coins = Items.new 75, 20..50
-    @dino_ups = Items.new 50, 100..100
+    @dino_ups = Items.new 500, 500..1000
   end
 end
